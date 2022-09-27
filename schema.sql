b7911f07948891cdb472d1baf2a2a9bf612455e8
Celeste T <celestet@Celestes-MacBook-Air.local>
2022-09-26 19:22:19 -0700

    creating table for departments

--- schema.sql
+++ schema.sql
@@ -3,3 +3,9 @@ DROP DATABASE IF EXISTS employee_trackerDB;
 CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
