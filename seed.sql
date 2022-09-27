bb7bce72bb7bbdea871590898a58bb46ebcc1d6c
Celeste T <celestet@Celestes-MacBook-Air.local>
2022-09-26 19:20:27 -0700

    adding employees to seeds

--- seed.sql
+++ seed.sql
@@ -26,3 +26,25 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("Legal Team Lead", 150000, 4 );
 INSERT INTO role (title, salary, department_id)
 VALUES ("Lawyer", 200000, 4 );
+
+-- employees 
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Justin", "Doe", 1);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Mike", "Chase", 2);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Chris", "Drew", 3);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Kevin", "Tone", 4);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Star", "Bass", 5);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Bob", "Allen", 6);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Chris", "Smith", 7);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Nick", "Erik", 8);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("AJ", "Sun", 2);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Harold", "Doe", 4);
