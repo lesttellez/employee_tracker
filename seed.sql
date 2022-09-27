6170545c24f9a3e58931bcc55c774541ab3e2f76
Celeste T <celestet@Celestes-MacBook-Air.local>
2022-09-26 19:13:10 -0700

    adding role to seeds

--- seed.sql
+++ seed.sql
@@ -8,3 +8,21 @@ INSERT INTO department (name)
 VALUES ("Finance");
 INSERT INTO department (name)
 VALUES ("Legal");
+
+-- roles
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1 );
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", 80000, 1 );
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 2 );
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 2 );
+INSERT INTO role (title, salary, department_id)
+VALUES ("Account Manager", 155000, 3 );
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 3 );
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 4 );
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 190000, 4 );
